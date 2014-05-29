8397e66adb5af6670f29a08ae6f067091b4ab70b
ChildhoodAndy <dabing1022@gmail.com>
2014-05-30 00:58:29 +0800

    add format test
    add format test

--- python/e010.py
+++ python/e010.py
@@ -6,5 +6,16 @@ b = float(a)
 print 'a type is ', type(a), ' value is ', a
 print 'b type is ', type(b), ' value is ', b
 
+c = 100.0234324
+print 'c int is %d' % c
+print 'c .2f is %.2f' % c
+print 'c is %+f' % c
+c = -1 * c
+print 'c is %+f' % c
+
+print 'c is % .2f' % c
+d = 200.234234
+print 'd is % .2f' % d
+
 print 'This is wrong, it will print <ValueError: could not convert string to float: ChildhoodAndy>'
 print float("ChildhoodAndy")
