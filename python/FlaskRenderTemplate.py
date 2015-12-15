7b8efbc2c99479dee6dd31f5cae81ad7674dd7ff
ChildhoodAndy <zhaoweibing@didapinche.com>
2015-12-15 19:51:33 +0800

    add flask render template

---
+++ python/FlaskRenderTemplate.py
@@ -0,0 +1,13 @@
+# coding:utf-8
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
+
+if __name__ == '__main__':
+    app.run()
